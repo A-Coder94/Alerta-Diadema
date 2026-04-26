09fcf4547ee5074a0a8ad05437ec1bc9a8f76177
Anderson de Souza <ac.coder94@gmail.com>
2026-04-26 19:38:15 -0300

    atualizacao de cors

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,12 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-app.use(cors()); 
 app.use(express.json()); 
+app.use(cors({
+  origin: '*', 
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type']
+}));
 
 // 1. CONEXÃO MONGODB
 mongoose.connect(process.env.MONGO_URI)
