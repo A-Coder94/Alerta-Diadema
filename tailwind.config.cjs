09c0e6122797798242d2a52b57aeaf5cdc588947
Anderson de Souza <ac.coder94@gmail.com>
2026-04-27 08:24:40 -0300

    fix: configurando content do tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,8 @@
- /** @type {import('tailwindcss').Config} */
-module.exports = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "./frontend/src/**/*.{js,ts,jsx,tsx}", // Adicionei essa linha por segurança
   ],
   theme: {
     extend: {},
